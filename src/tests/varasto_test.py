0a10202c07199560be82ff770d3bbf78e32c1632
hbuMi <miki2.julin@gmail.com>
2025-11-03 16:30:38 +0200

    unittest tehty

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,41 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_negatiivinen_tilavuus(self):
+        testi = Varasto(-1)
+        self.assertAlmostEqual(testi.tilavuus, 0)
+
+    def test_negatiivinen_saldo(self):
+        testi = Varasto(1, -1)
+        self.assertAlmostEqual(testi.saldo, 0)
+
+    def test_iso_saldo(self):
+        testi = Varasto(1, 2)
+        self.assertAlmostEqual(testi.saldo, testi.tilavuus)
+
+    def test_lisaa_maara_alle_nolla(self):
+        testi = Varasto(1, 0)
+        testi.lisaa_varastoon(-3)
+        self.assertAlmostEqual(testi.saldo, 0)
+
+    def test_maara_isompi(self):
+        self.varasto.lisaa_varastoon(100)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ota_maara_alle_nolla(self):
+        self.varasto.lisaa_varastoon(1)
+        saatu = self.varasto.ota_varastosta(-8)
+        self.assertAlmostEqual(saatu, 0)
+        self.assertAlmostEqual(self.varasto.saldo, 1)
+
+    def test_ota_enemman_kuin_saldo(self):
+        self.varasto.lisaa_varastoon(8)
+        saatu = self.varasto.ota_varastosta(80)
+        self.assertAlmostEqual(saatu, 8)
+        self.assertEqual(self.varasto.saldo, 0)
+
+    def test_str_metodi_palauttaa_oikean_merkkijonon(self):
+        self.varasto.lisaa_varastoon(5)
+        odotus = "saldo = 5, vielä tilaa 5"
+        self.assertEqual(str(self.varasto), odotus)
